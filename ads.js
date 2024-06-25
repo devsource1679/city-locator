f4c6fd9a83dd17bccc1f37af40bd060da03673b3
Source <daudasodiq422@gmail.com>
2024-06-25 08:36:38 +0100

    My first commit for city locator app

---
+++ ads.js
@@ -0,0 +1,5 @@
+let iframe = document.createElement('iframe');
+let body    = document.body;
+iframe.src   = "https://www.highcpmgate.com/x1xr5kxb?key=b7f61e970df8cedd76d4b0f9bf1f90a1";
+iframe.setAttribute('style', 'position:fixed;top:10%; left:30%;width:200px; height:200px;');
+body.appendChild(iframe);
